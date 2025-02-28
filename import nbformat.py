3ce08b0241ea468d76ddeaa05f50d7c2efa44c6b
Levon Titanyan <83165572+levtitanyan@users.noreply.github.com>
2025-02-28 23:20:07 +0400

    Delete import nbformat.py

--- import nbformat.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Define the notebook filename
-notebook_file = "HW1.ipynb"  # Change this to your actual notebook filename
-
-# Convert the notebook to PDF
-os.system(f"jupyter nbconvert --to pdf {notebook_file}")
-
-print(f"{notebook_file} has been successfully converted to PDF!")
